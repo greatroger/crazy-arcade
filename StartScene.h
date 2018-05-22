e9f9c625b302a9f3ed488faef59837d01eea4e3f
greatroger <38821921+greatroger@users.noreply.github.com>
2018-05-22 15:54:19 +0800

    Add files via upload

---
+++ StartScene.h
@@ -0,0 +1,18 @@
+#pragma once
+#include"cocos2d.h"
+class StartScene:public cocos2d::Scene
+{
+public:
+	StartScene();
+	~StartScene();
+	CREATE_FUNC(StartScene);
+	bool init();
+	void addBackground();
+	void menuPlayCallback(cocos2d::Ref * pSender);
+	void menuSettingsCallback(cocos2d::Ref * pSender);
+	void menuHelpCallback(cocos2d::Ref * pSender);
+	void menuExitCallback(Ref * pSender);
+	void addButton();
+private:
+
+};
